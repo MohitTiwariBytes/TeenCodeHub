3347ea2486a9d357bb68f00552eab758b7ee51d8
mohittiwariis97 <mohittiwariis97@gmail.com>
2024-06-26 10:25:23 +0000

    Fisrt Section completed

--- TeenCodeHub/src/Pages/LandingPage/LandingPage.jsx
+++ TeenCodeHub/src/Pages/LandingPage/LandingPage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./LandingPage.css";
 import Navbar from "../../Components/Navbar";
+import FirstSection from "./Sections/FirstSection";
 
 const LandingPage = () => {
   return (
     <div className="main-landing-page">
       <div className="landingPage">
         <Navbar></Navbar>
+        <FirstSection></FirstSection>
       </div>
     </div>
   );
---
+++ TeenCodeHub/src/Pages/LandingPage/Sections/FirstSection.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import gsap from "gsap";
+import "./FirstSection.css";
+
+const FirstSection = () => {
+  return (
+    <div className="first-section">
+      <h1>
+        A place built by a <z>Teenager</z> for a <z>Teenager</z> Where you can
+        collaborate with developers around the world!
+      </h1>
+    </div>
+  );
+};
+
+export default FirstSection;
