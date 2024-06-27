666ae89f5c05aa4e028ade1943f8dd6a9e2f5e52
mohittiwariis97 <mohittiwariis97@gmail.com>
2024-06-27 05:50:34 +0000

    Started Working on Login Page

--- TeenCodeHub/src/Pages/LoginPage/LoginPage.jsx
+++ TeenCodeHub/src/Pages/LoginPage/LoginPage.jsx
@@ -3,12 +3,28 @@ import "./LoginPage.css";
 import logo from "../../assets/newLogo.png";
 
 const LoginPage = () => {
+  console.log("22" + "22");
   return (
     <div className="mainLoginPage">
-      <div className="left">
-        <img src={logo} alt="" />
+      <div className="LoginPageLeft">
+        <div className="img">
+          <img src={logo} alt="" />
+        </div>
+        <div className="textLoginPage">
+          <h1>TeenCodeHub</h1>
+        </div>
       </div>
-      <div className="right"></div>
+      <div className="LoginPageRight">
+        <div className="formParent">
+          <div className="form">
+            <h1>SignUp!</h1>
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <input type="text" placeholder="Username" />
+            <button>Continue</button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
